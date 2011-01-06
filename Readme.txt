d2e8c67244e93846d02407cfe6e8da0abe55f340
jeanlouis.persat <jeanlouis.persat@6d232958-7d05-83cd-1d04-186d2947424c>
2011-01-06 08:57:07 +0000

    * change timer to FileSystemWatcher to update menu

--- Readme.txt
+++ Readme.txt
@@ -5,11 +5,12 @@ What LibraryDepot do:
 
     * Extract zip files to your src directory
     * Allow to place zip files in sub directories
+    * Copy swc files to your lib directory
 
-What TraceUtil does NOT do:
+What LibraryDepot does NOT do:
 
-    * Copy swc libraries
-	* Copy svn repositories
+    * Copy svn repositories
+    * Keep treemenu synced with depot, FD restart needed
 
 
 Thanks in advance to anyone who tries it out!
@@ -27,6 +28,15 @@ Known Bugs/Issues
 
 
 Version History
+
+    / 1.0.36 - January 6, 2010
+      * change timer to FileSystemWatcher to update menu
+
+    / 1.0.27 - January 5, 2010
+      + add filter for library files, only swc, zip files show up in treemenu
+      + add filters for zip extract
+      + dialog at first run to select depot path
+      * bug fix of treemenu refresh when library path setting changes
 
     / 1.0.5 - December 23, 2010
       + add support for SWC file
--- Settings.cs
+++ Settings.cs
@@ -15,7 +15,15 @@ using PluginCore;
 namespace LibraryDepot
 {
 	public delegate void SettingChangeHandler(string setting);
-	
+	/*public enum TimerCheckInterval
+	{
+		Never = 0,
+		Every30sec = 30000,
+		Every60sec = 60000,
+		Every2min = 120000,
+		Every5min = 300000
+	}*/
+
 	[Serializable]
     public class Settings
     {
@@ -24,10 +32,14 @@ namespace LibraryDepot
 		const string DEFAULT_LIBRARY_PATH = "";
 		const string DEFAULT_SRC_PATH = "/src/";
 		const string DEFAULT_SWC_PATH = "/lib/";
+		//const TimerCheckInterval DEFAULT_TIMER = TimerCheckInterval.Never;
+		const bool DEFAULT_ENABLE_WATCHER = true;
 
 		private string __librarypath = DEFAULT_LIBRARY_PATH;
 		private string __srcpath = DEFAULT_SRC_PATH;
 		private string __swcpath = DEFAULT_SWC_PATH;
+		//private TimerCheckInterval __checktimer = DEFAULT_TIMER;
+		private bool __enablewatcher = DEFAULT_ENABLE_WATCHER;
 
 		/// <summary> 
 		/// Get and sets the __librarypath
@@ -46,7 +58,7 @@ namespace LibraryDepot
 		}
 
 		/// <summary> 
-		/// Get and sets the __librarypath
+		/// Get and sets the __srcpath
 		/// </summary>
 		[DisplayName("Src path")]
 		[DefaultValue(DEFAULT_SRC_PATH)]
@@ -61,7 +73,7 @@ namespace LibraryDepot
 		}
 
 		/// <summary> 
-		/// Get and sets the __librarypath
+		/// Get and sets the __swcpath
 		/// </summary>
 		[DisplayName("Swc path")]
 		[DefaultValue(DEFAULT_SWC_PATH)]
@@ -72,6 +84,36 @@ namespace LibraryDepot
 			{
 				this.__swcpath = value;
 				FireChanged("SwcPath");
+			}
+		}
+
+		/// <summary> 
+		/// Get and sets the __checktimer
+		/// </summary>
+		/*[DisplayName("Check library path")]
+		[DefaultValue(DEFAULT_TIMER)]
+		public TimerCheckInterval CheckTimer
+		{
+			get { return this.__checktimer; }
+			set
+			{
+				this.__checktimer = value;
+				FireChanged("CheckTimer");
+			}
+		}*/
+
+		/// <summary> 
+		/// Get and sets the __checktimer
+		/// </summary>
+		[DisplayName("Enable library watcher")]
+		[DefaultValue(DEFAULT_ENABLE_WATCHER)]
+		public bool EnableWatcher
+		{
+			get { return this.__enablewatcher; }
+			set
+			{
+				this.__enablewatcher = value;
+				FireChanged("EnableWatcher");
 			}
 		}
 
